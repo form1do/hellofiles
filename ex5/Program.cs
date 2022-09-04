94cd6981d6545659fea7288722121ab8a969e014
Dmitry <spitsynfilmeffect@gmail.com>
2022-09-05 00:21:25 +0300

    add some changes

---
+++ ex5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Представься");
+string name = Console.ReadLine();
+if (name == "Дима")
+{
+    Console.WriteLine("Рад видеть, Дима");
+}
+else
+{
+Console.WriteLine("НУ привет, name");
+}
